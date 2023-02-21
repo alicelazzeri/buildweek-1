ccc113e34e3089f575cf5aac67a6e2690adde0f7
Luigi160397 <luigi.difraia.97@gmail.com>
2023-02-21 18:08:12 +0100

    test

--- progetto/assets/js/js.js
+++ progetto/assets/js/js.js
@@ -151,8 +151,8 @@ function creaElementoId(elemento, padre, id, testo) {
     function quiz (questions) {
 
         let domanda = creaElementoClasse('div','#container1','title','');
-        domanda.innerHTML = 
-    }
+        
+    };
 
     let link = document.querySelector('.link')
     link.addEventListener('click', () => {
