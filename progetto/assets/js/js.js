eb78b4146b53d44470aa9f0c400b3b0546e2a505
alicelazzeri <alice.lazzeri@hotmail.it>
2023-02-21 09:41:12 +0100

    array js

--- progetto/assets/js/js.js
+++ progetto/assets/js/js.js
@@ -1,3 +1,104 @@
+const questions = [
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "What does CPU stand for?",
+    correct_answer: "Central Processing Unit",
+    incorrect_answers: [
+      "Central Process Unit",
+      "Computer Personal Unit",
+      "Central Processor Unit",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+    correct_answer: "Final",
+    incorrect_answers: ["Static", "Private", "Public"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "The logo for Snapchat is a Bell.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question:
+      "Pointers were not used in the original C programming language; they were added later on in C++.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "What is the most preferred image format used for logos in the Wikimedia database?",
+    correct_answer: ".svg",
+    incorrect_answers: [".png", ".jpeg", ".gif"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "In web design, what does CSS stand for?",
+    correct_answer: "Cascading Style Sheet",
+    incorrect_answers: [
+      "Counter Strike: Source",
+      "Corrective Style Sheet",
+      "Computer Style Sheet",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "What is the code name for the mobile operating system Android 7.0?",
+    correct_answer: "Nougat",
+    incorrect_answers: [
+      "Ice Cream Sandwich",
+      "Jelly Bean",
+      "Marshmallow",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "On Twitter, what is the character limit for a Tweet?",
+    correct_answer: "140",
+    incorrect_answers: ["120", "160", "100"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "Linux was first created as an alternative to Windows XP.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "Which programming language shares its name with an island in Indonesia?",
+    correct_answer: "Java",
+    incorrect_answers: ["Python", "C", "Jakarta"],
+  },
+];
+
+
 let body = document.querySelector('body');
 
 
@@ -25,11 +126,11 @@ function creaElementoId(elemento, padre, id, testo) {
 
     
     creaElementoId('div','body','container1','');
-    creaElementoClasse('div','#container1','title','Welcome to ');
+    creaElementoClasse('h1','#container1','title','Welcome to ');
     creaElementoClasse('span','.title','bold','your exam');
     creaElementoId('div','#container1','container2','');
-    creaElementoId('div','#container2','subtitle','Instructions');
-    creaElementoClasse('div','#container2','paragraph',`We don't expect most engineers to know the answers to 
+    creaElementoId('h2','#container2','subtitle','Instructions');
+    creaElementoClasse('p','#container2','paragraph',`We don't expect most engineers to know the answers to 
     all of these questions, so don't worry if you're unsure of a few of them.`);
     creaElementoId('ul','#container1','lista1','');
     creaElemento('li','#lista1','Each question is <span class="b">timed</span> and can only be <span class="b">answered once</span>.');
@@ -43,6 +144,15 @@ function creaElementoId(elemento, padre, id, testo) {
     let label = document.querySelector('label');
     label.setAttribute('for', 'check');
     creaElementoClasse('a','#container3','link','PROCEED');
+
+
+
+    function quiz (questions) {
+      
+
+
+    }
+
     let link = document.querySelector('.link')
     link.addEventListener('click', () => {
 
