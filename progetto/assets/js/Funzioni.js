08717c6642481e7e089a40fa5a69b1f8ec1c26d8
Luigi160397 <luigi.difraia.97@gmail.com>
2023-02-22 12:56:10 +0100

    1domandaok

--- progetto/assets/js/Funzioni.js
+++ progetto/assets/js/Funzioni.js
@@ -17,3 +17,156 @@ export  function creaElementoId(elemento, padre, id, testo) {
         nuovoElemento.innerHTML = testo;
         document.querySelector(padre).append(nuovoElemento);
       }
+
+export function newAnswer(answerObj){    
+
+    const questions = [
+        {
+          category: "Science: Computers",
+          type: "multiple",
+          difficulty: "easy",
+          question: "What does CPU stand for?",
+          correct_answer: "Central Processing Unit",
+          incorrect_answers: [
+            "Central Process Unit",
+            "Computer Personal Unit",
+            "Central Processor Unit",
+          ],
+        },
+        {
+          category: "Science: Computers",
+          type: "multiple",
+          difficulty: "easy",
+          question:
+            "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+          correct_answer: "Final",
+          incorrect_answers: ["Static", "Private", "Public"],
+        },
+        {
+          category: "Science: Computers",
+          type: "boolean",
+          difficulty: "easy",
+          question: "The logo for Snapchat is a Bell.",
+          correct_answer: "False",
+          incorrect_answers: ["True"],
+        },
+        {
+          category: "Science: Computers",
+          type: "boolean",
+          difficulty: "easy",
+          question:
+            "Pointers were not used in the original C programming language; they were added later on in C++.",
+          correct_answer: "False",
+          incorrect_answers: ["True"],
+        },
+        {
+          category: "Science: Computers",
+          type: "multiple",
+          difficulty: "easy",
+          question:
+            "What is the most preferred image format used for logos in the Wikimedia database?",
+          correct_answer: ".svg",
+          incorrect_answers: [".png", ".jpeg", ".gif"],
+        },
+        {
+          category: "Science: Computers",
+          type: "multiple",
+          difficulty: "easy",
+          question: "In web design, what does CSS stand for?",
+          correct_answer: "Cascading Style Sheet",
+          incorrect_answers: [
+            "Counter Strike: Source",
+            "Corrective Style Sheet",
+            "Computer Style Sheet",
+          ],
+        },
+        {
+          category: "Science: Computers",
+          type: "multiple",
+          difficulty: "easy",
+          question:
+            "What is the code name for the mobile operating system Android 7.0?",
+          correct_answer: "Nougat",
+          incorrect_answers: [
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "Marshmallow",
+          ],
+        },
+        {
+          category: "Science: Computers",
+          type: "multiple",
+          difficulty: "easy",
+          question: "On Twitter, what is the character limit for a Tweet?",
+          correct_answer: "140",
+          incorrect_answers: ["120", "160", "100"],
+        },
+        {
+          category: "Science: Computers",
+          type: "boolean",
+          difficulty: "easy",
+          question: "Linux was first created as an alternative to Windows XP.",
+          correct_answer: "False",
+          incorrect_answers: ["True"],
+        },
+        {
+          category: "Science: Computers",
+          type: "multiple",
+          difficulty: "easy",
+          question:
+            "Which programming language shares its name with an island in Indonesia?",
+          correct_answer: "Java",
+          incorrect_answers: ["Python", "C", "Jakarta"],
+        },
+      ];
+    
+    
+          
+        fetch('template.html')
+        .then(res => res.text())
+        .then(res => {
+    
+                let target = document.querySelector('#target');
+                target.innerHTML = res
+                
+                let html = target.querySelector('#container1')
+                
+                
+               
+    
+            
+                
+    
+                //seleziono gli elementi
+                let titleDOM = html.querySelector('.title2')
+                let answersDOM = html.querySelector('#container2')
+                
+                
+                //inserisco contenuto
+                titleDOM.textContent = questions[0].question
+                console.log(titleDOM);
+                let risposte = [];
+
+                for(let val of questions[0].incorrect_answers){
+                    risposte.push(val)
+                }
+                risposte.push(questions[0].correct_answer)
+                
+                console.log(risposte);
+                for(let risp of risposte){
+
+                    let optionClone = html.querySelector('#container2 .button').cloneNode()
+                    optionClone.textContent = risp;
+
+                    answersDOM.append(optionClone);
+
+                }
+                
+
+                html.querySelector('#container2 .button').remove()
+                target.append(html)
+
+        })
+
+    }
+
