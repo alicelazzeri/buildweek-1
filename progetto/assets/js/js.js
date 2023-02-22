18d4c647f68cd6a40f3ead046f6edf01fa526673
Luigi160397 <luigi.difraia.97@gmail.com>
2023-02-22 16:04:38 +0100

    mediamente funzionante

--- progetto/assets/js/js.js
+++ progetto/assets/js/js.js
@@ -134,6 +134,7 @@ import {creaElementoId, creaElementoClasse, creaElemento, newAnswer} from "./Fun
         if(check.checked){
             container1.innerHTML='';
                 newAnswer(questions[indice]);
+                
             }
             else{
                 alert('confirm')
@@ -143,99 +144,15 @@ import {creaElementoId, creaElementoClasse, creaElemento, newAnswer} from "./Fun
     
       let indice = 0;
 
-        let next = document.getElementsByClassName('.button');
-        console.log(next);
-      for(let bottone of next){
-        bottone.addEventListener('click',function(){
-
-            newAnswer(questions[indice]);
-            indice++;
-      })}
+      let next = document.querySelectorAll('.button')
+      console.log(next);
+      for (let i = 0; i < next.length; i++) {
+        next[i].addEventListener('click', function() {
+          newAnswer(questions[indice]);
+          indice++;
+        })};
 
 
 
 //Timer
-/*const FULL_DASH_ARRAY = 283;
 
-
-const COLOR_CODES = {
-  info: {
-    color: "#00ffff"
-}
-}
-
-const TIME_LIMIT = 60;
-let timePassed = 0;
-let timeLeft = TIME_LIMIT;
-let timerInterval = null;
-let remainingPathColor = COLOR_CODES.info.color;
-
-document.getElementById("timer").innerHTML = `
-<div class="base-timer">
-  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-    <g class="base-timer__circle">
-      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-      <path
-        id="base-timer-path-remaining"
-        stroke-dasharray="283"
-        class="base-timer__path-remaining ${remainingPathColor}"
-        d="
-          M 50, 50
-          m 45, 0
-          a 45,45 0 1,0 -90,0
-          a 45,45 0 1,0 90,0
-        "
-      ></path>
-    </g>
-  </svg>
-  <span id="base-timer-label" class="base-timer__label">${formatTime(
-    timeLeft
-  )}</span>
-</div>
-`;
-
-startTimer();
-
-function onTimesUp() {
-  clearInterval(timerInterval);
-}
-
-function startTimer() {
-  timerInterval = setInterval(() => {
-    timePassed = timePassed += 1;
-    timeLeft = TIME_LIMIT - timePassed;
-    document.getElementById("base-timer-label").innerHTML = formatTime(
-      timeLeft
-    );
-    setCircleDasharray();
-    
-
-    if (timeLeft === 0) {
-      onTimesUp();
-    }
-  }, 1000);
-}
-
-function formatTime(time) {
-  const minutes = Math.floor(time / 60);
-  let seconds = time % 60;
-  if (seconds < 10) {
-    seconds = `0${seconds}`;
-  }
-
-return `${seconds}`;
-}
-
-function calculateTimeFraction() {
-  const rawTimeFraction = timeLeft / TIME_LIMIT;
-  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
-}
-
-function setCircleDasharray() {
-  const circleDasharray = `${(
-    calculateTimeFraction() * FULL_DASH_ARRAY
-  ).toFixed(0)} 283`;
-  document
-    .getElementById("base-timer-path-remaining")
-    .setAttribute("stroke-dasharray", circleDasharray);
-} */
